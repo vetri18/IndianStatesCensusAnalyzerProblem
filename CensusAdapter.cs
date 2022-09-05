c327190c196725fe92921cfd7fe92ce04b522111
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-05 14:43:59 +0530

    UC2.1 Number of Record matches

---
+++ CensusAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace IndianStateCensusAnalyser
+{
+    public abstract class CensusAdapter
+    {
+        protected string[] GetCensusData(string csvFilePath, string dataHeaders)
+        {
+            string[] censusData;
+            if (!File.Exists(csvFilePath))
+            {
+                throw new CensusAnalyserException("File not found", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
+
+            }
+            if (Path.GetExtension(csvFilePath) != ".csv")
+            {
+                throw new CensusAnalyserException("Invalid File type", CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
+
+            }
+            censusData = File.ReadAllLines(csvFilePath);
+            if (censusData[0] != dataHeaders)
+            {
+                throw new CensusAnalyserException("Incorrect header in Data", CensusAnalyserException.ExceptionType.INCORRECT_HEADER);
+            }
+            return censusData;
+
+        }
+    }
+}
