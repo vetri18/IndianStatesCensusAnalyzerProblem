24b6bbe76981550086a2ef571d5d0d231a288f4d
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-05 14:41:32 +0530

    UC2 Indian States Code Information from a csv file

---
+++ CsvAdapterFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CsvAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case CensusAnalyser.Country.INDIA:
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
---
+++ IndianStateCode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyser.Constructor_for_different_CSV
+{
+    public class IndianStateCode
+    {
+        public string stateName;
+        public string stateCode;
+        public int tin;
+        public int serialNumber;
+
+        public IndianStateCode(string stateName, string stateCode, string tin, string serialNumber)
+        {
+            this.stateName = stateName;
+            this.stateCode = stateCode;
+            this.tin = Convert.ToInt32(tin);
+            this.serialNumber = Convert.ToInt32(serialNumber);
+
+        }
+    }
+
+
+}
