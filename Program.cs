89b4db20013fbe674d08788e1402ef31246aadd8
vetrivel <vetrivel.kce@gmail.com>
2022-09-05 13:58:48 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStateCensusAnalyser
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser program using OOPS concepts");
+        }
+    }
+}
