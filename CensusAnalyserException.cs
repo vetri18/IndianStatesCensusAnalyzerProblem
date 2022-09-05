559e207b73bb0e80c4d890675c6bc15bb3d595b5
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-05 14:48:43 +0530

    UC2.4 delimiter incorrect Returns custom Exception

---
+++ CensusAnalyserException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CensusAnalyserException : Exception
+    {
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND, INVALID_FILE_TYPE, INCORRECT_DELIMITER, NO_SUCH_COUNTRY, INCORRECT_HEADER
+        }
+        public ExceptionType etype;
+        public CensusAnalyserException(string message, ExceptionType etype) : base(message)
+        {
+            this.etype = etype;
+        }
+    }
+}
