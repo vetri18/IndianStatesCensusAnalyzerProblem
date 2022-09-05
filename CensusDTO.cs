4b815056c5b103844cb14f22c5f765968f24f494
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-05 14:31:21 +0530

    UC1.1 Number of Record matches

---
+++ CensusDTO.cs
@@ -0,0 +1,36 @@
+﻿using IndianStateCensusAnalyser.Constructor_for_different_CSV;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public long population;
+        public long area;
+        public long density;
+        public string state;
+        public string stateCode;
+        public int tin;
+
+        public CensusDTO(IndianStateCensusData indianStateCensusData)
+        {
+            this.state = indianStateCensusData.state;
+            this.population = indianStateCensusData.population;
+            this.area = indianStateCensusData.area;
+            this.density = indianStateCensusData.density;
+        }
+        public CensusDTO(IndianStateCode indianStateCode)
+        {
+            this.serialNumber = indianStateCode.serialNumber;
+            this.stateName = indianStateCode.stateName;
+            this.stateCode = indianStateCode.stateCode;
+            this.tin = indianStateCode.tin;
+        }
+
+
+    }
+}
